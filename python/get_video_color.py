7eec5a46f414c66b4e49a0e7b96f04b991fa1007
Daniel M. Lofaro <dan@danlofaro.com>
2022-08-01 18:44:59 -0400

    added color to control the filter location and a square instead of a sin wave, sounds weird and good

---
+++ python/get_video_color.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+import time
+import sc_send as scs
+
+vid = cv2.VideoCapture('vtx250.jpg')
+#vid = cv2.VideoCapture('starry_night.jpg')
+ret, frame_orig = vid.read()
+height, width, dim = frame_orig.shape
+print( "height = ", height )
+print( "width  = ", width  )
+print( "dim    = ", dim    )
+#frame = frame_orig
+
+scs.FREQ_STEP = 128
+
+new_height = int(scs.FREQ_STEP)
+new_width  = int(width)
+#new_width  = int(width * new_height / height)
+frame = cv2.resize(frame_orig, ( new_width, new_height), interpolation = cv2.INTER_LINEAR)
+
+height, width, dim = frame.shape
+print( "new height = ", height )
+print( "new width  = ", width  )
+print( "new dim    = ", dim    )
+print(frame.shape)
+
+#img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+scs.FREQ_MIN=400.0
+scs.FREQ_MAX=1000.0
+
+scs.init()
+
+
+f_filt_center = 2000.0
+f_filt_swing  = 1500.0
+f_filt_freq   = 0.5
+
+for i in range(width):
+  col            = frame[:,i,0]
+  col_filt       = frame[:,i,1]
+  col_filt_width = frame[:,i,2]
+  for j in range(len(col)):
+    freq = j
+    freq_amp = col[j] / 255.0 / scs.FREQ_STEP
+
+    filt_freq = col_filt[j] / 255.0 * f_filt_center
+
+    scs.send(freq,freq_amp, 100.0, filt_freq)
+    #print(freq, " " , freq_amp)
+    print(".",end='')
+  print()
+  time.sleep(0.1)
+
+scs.off_all()  
