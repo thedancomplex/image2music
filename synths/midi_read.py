0e18fcb94b435cadccd0497e72bbec3793ba0731
Daniel M. Lofaro <dan@danlofaro.com>
2022-08-02 18:09:08 -0400

    tyring to play midi from python

---
+++ synths/midi_read.py
@@ -0,0 +1,36 @@
+import mido
+import time
+import rtmidi
+midiout = rtmidi.MidiOut()
+mid  = mido.MidiFile('radiohead-creep.mid')
+#port = mido.open_output('USB Uno MIDI Interface MIDI 1')
+#avaliable_ports = midiout.get_ports()
+#print(avaliable_ports)
+#port = midiout.open_port(0)
+
+ports = mido.get_output_names()
+print(ports)
+port = mido.open_output(ports[0])
+
+msg = mido.Message('note_on', note=60)
+# port.send(msg)
+
+print(port)
+for i in range(128):
+  msg = mido.Message('note_off', note=i)
+#  port.send_message(msg)
+  port.send(msg)
+
+for i, track in enumerate(mid.tracks):
+  print('Track {}: {}'.format(i, track.name))
+
+## print('pause 3 sec')
+## time.sleep(3.0)
+## print('play')
+for msg in mid.play():
+  print(msg)
+  port.send(msg)
+  #port.send_message(msg)
+
+
+
