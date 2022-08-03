7c94a70f41ff2788e02671377f77ec74a95762d9
Daniel M. Lofaro <dan@danlofaro.com>
2022-08-03 16:00:07 -0400

    now picking the tip 6 peaks to make notes.... does not sound good

--- python/get_video_keys_color.py
+++ python/get_video_keys_color.py
@@ -13,6 +13,7 @@ print( "dim    = ", dim    )
 #frame = frame_orig
 
 scs.FREQ_STEP = 128
+NOTE_OFFSET = 0
 
 new_height = int(scs.FREQ_STEP)
 new_width  = int(width)
@@ -38,18 +39,25 @@ f_filt_swing  = 1500.0
 f_filt_width  = 0.05
 f_filt_freq   = 0.5
 
+NUM_NOTES = 6
+
 for i in range(width):
   col            = frame[:,i,0]
   col_filt       = frame[:,i,1]
   col_filt_width = frame[:,i,2]
+  col_max        = np.argpartition(col, -NUM_NOTES)[-NUM_NOTES:] 
   for j in range(len(col)):
     freq = j
     freq_amp = col[j] / 255.0 / scs.FREQ_STEP
 
     filt_freq = col_filt[j] / 255.0 * f_filt_center
     filt_width = col_filt_width[j] / 255.0 * f_filt_width
+    if( j in col_max ):
+      scs.send_note(NOTE_OFFSET+j,freq_amp, 100.0)
+      #scs.send_note(NOTE_OFFSET+j,freq_amp, 100.0, filt_freq, filt_width)
+    else:
+      scs.send_note(NOTE_OFFSET+j,0.0)
 
-    scs.send(freq,freq_amp, 100.0, filt_freq)
     #scs.send(freq,freq_amp, 100.0, filt_freq, filt_width)
     #print(freq, " " , freq_amp)
     print(".",end='')
--- python/sc_send.py
+++ python/sc_send.py
@@ -55,7 +55,8 @@ def send_note_major(note_index=None, note_amp=None, post_gain=1.0, key_index=0):
 
 
 
-def send_note(note_index=None, note_amp=None, post_gain=1.0):
+#def send_note(note_index=None, note_amp=None, post_gain=1.0):
+def send_note(note_index=None, note_amp=None, post_gain=1.0, filter_freq=None, filter_width=None):
   global client 
   if note_index == None:
     return 1
@@ -70,5 +71,10 @@ def send_note(note_index=None, note_amp=None, post_gain=1.0):
     freq = nf.NOTES[note_index]
   except:
     return 1
-  client.send_message(final, (freq, note_amp*post_gain)) # set the frequency at 440
+  if(filter_freq == None):
+    client.send_message(final, (freq, note_amp*post_gain)) # set the frequency at 440
+  elif(filter_width==None):
+    client.send_message(final, (freq, note_amp*post_gain, filter_freq)) # set the frequency at 440
+  else:
+    client.send_message(final, (freq, note_amp*post_gain, filter_freq, filter_width)) # set the frequency at 440
   return 0
